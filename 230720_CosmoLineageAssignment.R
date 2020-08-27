091d428c8b88501f69c75f6cc7bc97da021cedf3
Kathryn Campbell <k.campbell.1@research.gla.ac.uk>
2020-08-27 14:16:38 +0100

    Rename and nice plot

--- 230720_CosmoLineageAssignment.R
+++ 230720_CosmoLineageAssignment.R
@@ -17,6 +17,8 @@ library(phylotate)
 library(caper)
 library(ggtree)
 library(stringr)
+library(pracma)
+library(ggrepel)
 
 
 
@@ -346,6 +348,19 @@ tree<-ggtree(Cosmotree) %<+% lineage_assignments +
   geom_tippoint(aes(colour = (cluster)))
 # initial plot of the tree; need to see this to understand the lineage names
 
+
+lineage_assignments$previous<-NA
+
+for (i in 1:567) {
+  lineage_assignments$previous[i]<-
+    Cosmometa$alignment.displayName[which(Cosmometa$sequence.sequenceID == lineage_assignments$tip[i])]
+}
+# Look at the previous lineage assigned to each sequence
+# Gone through by hand and identified which previous lineages are present in each new cluster - probably a way to do this in R
+# The first time a previous lineage appears in a cluster on it's own, write it down and check that everything descended from it is in that previous lineage
+# If so, assign that cluster as the previous lineage (seen below), and then assign further from here
+# Many previous lineages are missing from this (not in a cluster on their own)
+
 nodes_diff$cluster[15]<-"AF1a_A1"
 nodes_diff$cluster[5]<-"AF1b_A1"
 nodes_diff$cluster[73]<-"AF4_A1"
@@ -404,12 +419,176 @@ for (i in 47:75) {
   tree<-tree +
     geom_cladelabel(nodes_diff$Node[i], nodes_diff$cluster[i], offset = 0.057, offset.text = 0)
 }
+# The clade bars all need to be offset by a different amount so can't see a way to automate this
 
 tree
+# Plot with everything on it!
 
-lineage_assignments$previous<-NA
 
-for (i in 1:567) {
-  lineage_assignments$previous[i]<-
-    Cosmometa$alignment.displayName[which(Cosmometa$sequence.sequenceID == lineage_assignments$tip[i])]
+
+#############################################
+#         RENAME THE LINEAGES               #
+#############################################
+
+# Just doing this by eye currently, must be a better way
+
+nodes_diff$cluster[1]<-c("A1")
+nodes_diff$cluster[2]<-"B1"
+nodes_diff$cluster[3]<-"A1.1"
+nodes_diff$cluster[4]<-"A1.1.1"
+nodes_diff$cluster[5]<-"AF1b_A1"
+nodes_diff$cluster[6]<-"ME1a_A1"
+nodes_diff$cluster[7]<-"AF1b_B1"
+nodes_diff$cluster[8]<-"C1"
+nodes_diff$cluster[9]<-"AF1b_B1.1"
+nodes_diff$cluster[10]<-"C1.1"
+nodes_diff$cluster[11]<-"B1.1"
+nodes_diff$cluster[12]<-"D1"
+nodes_diff$cluster[13]<-"ME2_A1"
+nodes_diff$cluster[14]<-"AF1b_B1.1.1"
+nodes_diff$cluster[15]<-"AF1a_A1"
+nodes_diff$cluster[16]<-"CA1_A1"
+nodes_diff$cluster[17]<-"AF1b_A1.1"
+nodes_diff$cluster[18]<-"AF1b_A1.2"
+nodes_diff$cluster[19]<-"AF1b_A1.2.1"
+nodes_diff$cluster[20]<-"AF1b_C1"
+nodes_diff$cluster[21]<-"AF1b_A1.1.1"
+nodes_diff$cluster[22]<-"B1.2"
+nodes_diff$cluster[23]<-"AF1b_D1"
+nodes_diff$cluster[24]<-"AF1b_E1"
+nodes_diff$cluster[25]<-"AF1b_A1.3"
+nodes_diff$cluster[26]<-"E1"
+nodes_diff$cluster[27]<-"AF1b_D1.1"
+nodes_diff$cluster[28]<-"AF1b_A1.1.2"
+nodes_diff$cluster[29]<-"AF1b_E1.1"
+nodes_diff$cluster[30]<-"AF1b_F1"
+nodes_diff$cluster[31]<-"AF1b_A1.2.2"
+nodes_diff$cluster[32]<-"AF1b_A1.3.1"
+nodes_diff$cluster[33]<-"AF1b_A1.4"
+nodes_diff$cluster[34]<-"AF1b_A1.5"
+nodes_diff$cluster[35]<-"B1.3"
+nodes_diff$cluster[36]<-"AF1b_C1.1"
+nodes_diff$cluster[37]<-"AF1b_G1"
+nodes_diff$cluster[38]<-"AF1b_B1.1.2"
+nodes_diff$cluster[39]<-"AF1b_B1.1.3"
+nodes_diff$cluster[40]<-"AF1b_B1.2"
+nodes_diff$cluster[41]<-"B1.2.1"
+nodes_diff$cluster[42]<-"B1.2.2"
+nodes_diff$cluster[43]<-"B1.3"
+nodes_diff$cluster[44]<-"AF1a_A1.1"
+nodes_diff$cluster[45]<-"AF1a_A1.2"
+nodes_diff$cluster[46]<-"AF1a_A1.3"
+nodes_diff$cluster[47]<-"B1.1.1"
+nodes_diff$cluster[48]<-"ME1a_A1.1"
+nodes_diff$cluster[49]<-"ME1a_A1.2"
+nodes_diff$cluster[50]<-"ME1a_A1.3"
+nodes_diff$cluster[51]<-"ME1a_A1.4"
+nodes_diff$cluster[52]<-"ME1a_A1.5"
+nodes_diff$cluster[53]<-"ME1a_A1.6"
+nodes_diff$cluster[54]<-"ME1a_A1.7"
+nodes_diff$cluster[55]<-"ME1a_A1.8"
+nodes_diff$cluster[56]<-"ME1a_A1.9"
+nodes_diff$cluster[57]<-"ME1a_A1.10"
+nodes_diff$cluster[58]<-"CE_A1"
+nodes_diff$cluster[59]<-"C1.1.1"
+nodes_diff$cluster[60]<-"WE_A1"
+nodes_diff$cluster[61]<-"C1.1.2"
+nodes_diff$cluster[62]<-"EE_A1"
+nodes_diff$cluster[63]<-"NEE_A1"
+nodes_diff$cluster[64]<-"CA1_A1.1"
+nodes_diff$cluster[65]<-"CA1_A1.2"
+nodes_diff$cluster[66]<-"D1.1"
+nodes_diff$cluster[67]<-"ME2_A1.1"
+nodes_diff$cluster[68]<-"ME2_A1.2"
+nodes_diff$cluster[69]<-"A1.1.2"
+nodes_diff$cluster[70]<-"A1.1.3"
+nodes_diff$cluster[71]<-"CA2_A1"
+nodes_diff$cluster[72]<-"AM3a_A1"
+nodes_diff$cluster[73]<-"AF4_A1"
+nodes_diff$cluster[74]<-"E1.1"
+nodes_diff$cluster[75]<-"AM2a_A1"
+
+# Renamed according to Rambaut et al (2020) with A1.1.1.1 becoming a new letter (e.g. C1)
+
+lineage_assignments$new_cluster<-NA
+# For some reason it won't just overwrite the 'cluster' column like in the other scripts, so do this instead
+
+for (i in 1:75) {
+  lineage_assignments$new_cluster[which(lineage_assignments$cluster == i)]<-nodes_diff$cluster[i]
 }
+# Rename the lineages in the sequence assignment table
+
+names(lineage_assignments)<-c('tip', 'number', 'previous', 'cluster')
+
+names(lineage_assignments)
+
+
+
+
+#############################################
+#             REPLOT THE TREE               #
+#############################################
+attach(Cosmotree)
+
+# Make a nice figure to save! Similar to before but with text sizes, legend etc
+tree<-ggtree(Cosmotree) %<+% lineage_assignments +
+  geom_tippoint(na.rm = T, aes(colour = (cluster))) +
+  theme(legend.position = c(0.17, 0.83),
+        legend.text = element_text(size = 20),
+        legend.title = element_text(size = 20)) +
+  guides(colour=guide_legend(override.aes=list(alpha=1, size=5)))
+
+for (i in 1:5) {
+  tree<-tree +
+    geom_cladelabel(nodes_diff$Node[i], nodes_diff$cluster[i], offset = 0.006*i, offset.text = 0)
+}
+for (i in 6) {
+  tree<-tree +
+    geom_cladelabel(nodes_diff$Node[i], nodes_diff$cluster[i], offset = 0.018+0.003*i, offset.text = 0)
+}
+for (i in 7:10) {
+  tree<-tree +
+    geom_cladelabel(nodes_diff$Node[i], nodes_diff$cluster[i], offset = 0.008+0.003*i, offset.text = 0)
+}
+for (i in 11) {
+  tree<-tree +
+    geom_cladelabel(nodes_diff$Node[i], nodes_diff$cluster[i], offset = 0.014, offset.text = 0)
+}
+for (i in 14) {
+  tree<-tree +
+    geom_cladelabel(nodes_diff$Node[i], nodes_diff$cluster[i], offset = 0.008+0.003*i, offset.text = 0)
+}
+for (i in c(12, 13, 15, 16, 17, 18, 21, 23,24)) {
+  tree<-tree +
+    geom_cladelabel(nodes_diff$Node[i], nodes_diff$cluster[i], offset = 0.0025*i, offset.text = 0)
+}
+for (i in c(19,20)) {
+  tree<-tree +
+    geom_cladelabel(nodes_diff$Node[i], nodes_diff$cluster[i], offset = 0.0027*i, offset.text = 0)
+}
+for (i in c(22, 25)) {
+  tree<-tree +
+    geom_cladelabel(nodes_diff$Node[i], nodes_diff$cluster[i], offset = 0.0017*i, offset.text = 0)
+}
+for (i in 26) {
+  tree<-tree +
+    geom_cladelabel(nodes_diff$Node[i], nodes_diff$cluster[i], offset = 0.015, offset.text = 0)
+}
+for (i in 27:46) {
+  tree<-tree +
+    geom_cladelabel(nodes_diff$Node[i], nodes_diff$cluster[i], offset = 0.07, offset.text = 0)
+}
+for (i in 47:75) {
+  tree<-tree +
+    geom_cladelabel(nodes_diff$Node[i], nodes_diff$cluster[i], offset = 0.05, offset.text = 0)
+}
+# The clade bars all need to be offset by a different amount so can't see a way to automate this
+
+tree
+# Plot with everything on it!
+
+
+ggsave("figures/LineageTree.png", 
+       plot = last_plot(),
+       height = 15, width = 25)
+# Save it
